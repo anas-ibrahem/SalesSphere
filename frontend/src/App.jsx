1b9fa82b16b6de587850f79843f26f188a52cb91
Abdallah Ayman <149713232+AbdallahAyman03@users.noreply.github.com>
2024-11-29 14:03:40 +0200

    Add landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import Home from "./pages/Home"
-import { Navigate, Route , Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        {/* <Toaster /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
+      {/* <Toaster /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
